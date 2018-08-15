ce3c129fddd5345fa26a8dcd3f6c47e3339592e4
rakeshshriwas <rakesh.shriwas88@gmail.com>
2018-08-15 12:34:39 +0530

    update react state

--- src/App.js
+++ src/App.js
@@ -8,15 +8,26 @@ class App extends Component {
       first_name: "Rakesh",
       last_name: "Shriwas",
     }
+
+    this.updateData = this.updateData.bind(this);
+  }
+
+  updateData() {
+    this.setState({ 
+        first_name: "Manoj",
+        last_name: "Pawar"
+      });
   }
 
   render() {
-    return ( 
-      <div style = {{textAlign: "center"}}>
+    return (
+      <div style={{ textAlign: "center" }}>
         <p>First Name : {this.state.first_name}</p>
         <p>Last Name : {this.state.last_name}</p>
+
+        <button onClick={this.updateData}>Update Data</button>
       </div>
-     );
+    );
   }
 }
 
